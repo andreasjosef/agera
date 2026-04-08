f0ea90f210db022a35c1b664e0b7494c0b3a6d94
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-08 18:51:22 +0200

    feat: add GET request requirement logic for controller & service

--- apps/express/src/controllers/requirements.controller.ts
+++ apps/express/src/controllers/requirements.controller.ts
@@ -1,10 +1,13 @@
 
 
 import type { Request, Response } from "express"
+import { requirementsService } from "../services/requirements.service.ts"
 
 
+export const getRequirements = async (req: Request, res: Response) => {
+    const result = await requirementsService()
 
+    if(!result.ok) return res.status(500).json(result)
 
-export const getRequirements = (req: Request, res: Response) => {
-    
+    return res.json(result)
 }
