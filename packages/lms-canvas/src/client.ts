76f1afed489d9adf2b81769d7e4170c12f4a365e
Andreas Stratmann <hello.andreas@protonmail.com>
2026-04-11 14:00:10 +0200

    feat(pkg): implement the canvas client

--- packages/lms-canvas/src/client.ts
+++ packages/lms-canvas/src/client.ts
@@ -1,38 +1,85 @@
 import { fetchList, zodRawParser } from "@ccpilot/ts-fetch";
-import { CANCourseObjectSchema, CANAssignmentObjectSchema } from "./schema.ts";
+
+import {
+  type CanvasCourse,
+  type CanvasAssignment,
+  type CanvasClientInterface,
+  ok,
+  fail,
+} from "@ccpilot/domain";
+
+import {
+  CanvasCourseResponseSchema,
+  CANAssignmentObjectSchema,
+  type CanvasResponseAssignment,
+} from "./schema.ts";
 
 const BASE_URL = "https://chasacademy.instructure.com/api/v1";
-const CANVAS_TOKEN =
-  "19606~6tZeKACyPGCVM6v4nyneHL773mfBkYN99QXwJDNvxURTvMkLJBPVY4DXHBQZ2x6D";
 
-const result = await fetchList(
-  `${BASE_URL}/courses`,
-  zodRawParser(CANCourseObjectSchema),
-  {
-    headers: {
-      Authorization: `Bearer ${CANVAS_TOKEN}`,
+export const createCanvasClient = (apiKey: string): CanvasClientInterface => {
+  return {
+    fetchAssignments: async (courseId) => {
+      // TODO: Refacotr the whole fetch logic out into ./api.ts
+      const result = await fetchList<CanvasResponseAssignment>(
+        `${BASE_URL}/courses/${courseId}/assignments?per_page=40`,
+        zodRawParser(CANAssignmentObjectSchema),
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+          // onItemError: (err, item) => {
+          //   console.log("Skipped an item due to parsing error", { item, err });
+          // },
+        },
+      );
+
+      if (!result.ok) return fail(result.error);
+
+      const assignments: CanvasAssignment[] = [];
+
+      // mapping the response type to domain type
+      // TODO: this should be moved to a mapper function for easier updating and fixing
+      result.value.forEach((assignment) => {
+        assignments.push({
+          id: assignment.id.toString(),
+          title: assignment.name,
+          due: assignment.due_at,
+          description: assignment.description,
+        });
+      });
+
+      return ok(assignments);
     },
-    onItemError: (err, item) => {
-      console.log("Skipped an item due to parsing error", { err, item });
+
+    fetchCourses: async () => {
+      // TODO: Refactor the whole fetch logic out into ./api.ts
+      const result = await fetchList(
+        `${BASE_URL}/courses`,
+        zodRawParser(CanvasCourseResponseSchema),
+        {
+          headers: {
+            Authorization: `Bearer ${apiKey}`,
+          },
+          // onItemError: (err, item) => {
+          //   console.log("Skipped an item due to parsing error", { item, err });
+          // },
+        },
+      );
+
+      if (!result.ok) return fail(result.error);
+
+      const courses: CanvasCourse[] = [];
+
+      // mapping the response type to domain type
+      // TODO: this should be moved to a mapper function for easier updating and fixing
+      result.value.forEach((course) =>
+        courses.push({
+          id: course.id.toString(),
+          calendar: course.calendar.ics,
+        }),
+      );
+
+      return ok(courses);
     },
-  },
-);
-
-if (!result.ok) throw new Error(result.error);
-
-result.value.forEach(async (course) => {
-  const result = await fetchList(
-    `${BASE_URL}/courses/${course.id}/assignments?per_page=40`,
-    zodRawParser(CANAssignmentObjectSchema),
-    {
-      headers: {
-        Authorization: `Bearer ${CANVAS_TOKEN}`,
-      },
-      onItemError: (err, item) => {
-        console.log("Skipped an item due to parsing error", { err, item });
-      },
-    },
-  );
-
-  console.log(result);
-});
+  };
+};
