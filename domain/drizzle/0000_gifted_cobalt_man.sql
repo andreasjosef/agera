b1709a9b2a8bc6a1cf1935ff3c1ef0d2cd8c36f4
Andreas Stratmann <hello.andreas@protonmail.com>
2026-04-14 08:56:36 +0200

    chore(root): remove drizzle ghost dir form root

--- domain/drizzle/0000_gifted_cobalt_man.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE "public"."requirementType" AS ENUM('assignment', 'lecture', 'examn', 'admin');--> statement-breakpoint
-CREATE TABLE "requirements" (
-	"requirement_id" serial PRIMARY KEY NOT NULL,
-	"type" "requirementType" NOT NULL
-);
