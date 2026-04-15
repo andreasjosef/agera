4d4db6482e3c38d784e7d9caf4442d0b25f50924
Andreas Stratmann <hello.andreas@protonmail.com>
2026-04-15 21:49:40 +0200

    fix(domain): use newRequirment type in save action

--- packages/domain/src/requirements/actions.ts
+++ packages/domain/src/requirements/actions.ts
@@ -1,5 +1,5 @@
 import { type Result } from "../shared/result.ts";
-import type { Requirement, Step } from "./types.ts";
+import type { NewRequirement, Requirement, Step } from "./types.ts";
 
 import type { IRequirementRepository } from "./repository.ts";
 import { type LLMClientInterface } from "../services/llm.ts";
@@ -16,7 +16,7 @@ export const getRequirements = async (
 
 export const saveRequirement = async (
   repo: IRequirementRepository,
-  req: Requirement, // NOTE: This might not be a full requirement yet in reality ie just NewReq or so and we need to fill in the rest in here like id etc
+  req: NewRequirement, // NOTE: This might not be a full requirement yet in reality ie just NewReq or so and we need to fill in the rest in here like id etc
 ): Promise<Result<Requirement>> => {
   // TODO: some validation will have to happen here like
   //  - does this already exist in the db
