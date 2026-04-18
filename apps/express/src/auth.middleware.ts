5b6baea7a86ca784e2f796fd3e0011ccd533c9e1
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-04-18 12:27:16 +0200

    feat(express) add auth middleware

---
+++ apps/express/src/auth.middleware.ts
@@ -0,0 +1,24 @@
+import { authService } from '@ccpilot/auth-betterauth'
+import { Response, NextFunction } from 'express'
+import { AuthRequest } from '@ccpilot/domain'
+
+interface RequestWithUser extends AuthRequest {
+    user: {
+        id: string;
+        name: string;
+        email: string;
+    }
+} 
+
+export const authenticateUser = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+    
+    const result = await authService.getSession(req)
+
+    if(!result.ok) {
+        return res.status(401).json({message: result.error})
+    }
+
+    req.user = result.value
+
+    next()
+}
