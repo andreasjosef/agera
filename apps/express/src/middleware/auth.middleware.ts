58b95f01cbd9e1ccb68d69bab98bf7e60cbd4c2c
Andreas Stratmann <hello.andreas@protonmail.com>
2026-04-20 13:40:03 +0200

    fix(express): extend request with user id

--- apps/express/src/middleware/auth.middleware.ts
+++ apps/express/src/middleware/auth.middleware.ts
@@ -1,22 +1,23 @@
-import { authService } from '@ccpilot/auth-betterauth'
-import { Response, NextFunction } from 'express'
-import { AuthRequest } from '@ccpilot/domain'
+import { authService } from "@ccpilot/auth-betterauth";
+import { fail } from "@ccpilot/domain";
+import type { Response, Request, NextFunction } from "express";
 
-interface RequestWithUser extends AuthRequest {
-    user: {
-        id: string;
-        name: string;
-        email: string;
-    }
-} 
-
-export const authenticateUser = async (req: RequestWithUser, res: Response, next: NextFunction) => {
-    
-    const result = await authService.getSession(req)
-
-    if(!result.ok) return res.status(401).json({message: result.error})
-    
-    req.user = result.value
-
-    next()
+export interface RequestWithUser extends Request {
+  userid: string;
 }
+
+export const authenticateUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const result = await authService.getSession({
+    headers: req.headers as Record<string, string>,
+  });
+
+  if (!result.ok) return res.status(401).json(fail(result.error));
+
+  (req as RequestWithUser).userid = result.value.id;
+
+  next();
+};
