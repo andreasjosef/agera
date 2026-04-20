d105afa801070322a696de7db5fd6740b74f5a8b
Andreas Stratmann <hello.andreas@protonmail.com>
2026-04-20 13:06:32 +0200

    feat(express): add cookie-parser middleware

--- apps/express/src/index.ts
+++ apps/express/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { ok } from "@ccpilot/domain";
 
 import { authHandlerNode } from "@ccpilot/auth-betterauth";
@@ -14,8 +15,13 @@ const app = express();
 
 app.all("/api/auth/*splat", authHandlerNode);
 
+app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:3000/"],
+  }),
+);
 
 app.get("/health", (req, res) => {
   res.status(200).json(ok("Up and running !"));
