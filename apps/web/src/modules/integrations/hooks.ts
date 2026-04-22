00268fe43833159fd3114ae4230ed1efa5a87384
Daniil Gurski <daniilgurski6@gmail.com>
2026-04-22 22:13:48 +0200

    feat(web): invalidate auth session on sucessfull connection

--- apps/web/src/modules/integrations/hooks.ts
+++ apps/web/src/modules/integrations/hooks.ts
@@ -1,11 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { integrationMutations } from "./api";
+import { useNavigate } from "@tanstack/react-router";
 
 export const useCanvasConnect = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+
   const { mutate, isPending, data } = useMutation({
     mutationFn: (data: unknown) => integrationMutations.connectCanvas(data),
     onSuccess: () => {
-      // TODO: Do we need to handle something here ?
+      queryClient.invalidateQueries({ queryKey: ["auth", "session"] });
+      navigate({ to: "/app/dashboard" });
     },
   });
 
