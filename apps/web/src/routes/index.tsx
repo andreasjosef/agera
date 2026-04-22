7b762c445fbf494116adecb20c9dde901ddc7808
Daniil Gurski <daniilgurski6@gmail.com>
2026-04-22 14:33:51 +0200

    chore(web): implement basic index page layout, redirect user to main app if session exists

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,8 +1,24 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { authQueries } from "@/modules/auth/api";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
+  beforeLoad: async ({ context }) => {
+    const { queryClient } = context;
+
+    try {
+      const user = await queryClient.ensureQueryData(authQueries.session());
+
+      if (user) {
+        throw redirect({
+          to: "/app",
+        });
+      }
+    } catch (e) {
+      // Keep user on the landing page
+    }
+  },
 });
 
 function RouteComponent() {
@@ -10,10 +26,18 @@ function RouteComponent() {
     <>
       <header className="flex justify-between">
         <h1 className="text-2xl font-bold">CCPILOT</h1>
+
         <Link className="font-medium" to="/login">
           Login
         </Link>
       </header>
+
+      <main className="min-h-screen">
+        <h2 className="text-xl font-medium"> Introducing CCPilot </h2>
+        <p> Executive function as a service for students </p>
+      </main>
+
+      <footer>Footer</footer>
     </>
   );
 }
