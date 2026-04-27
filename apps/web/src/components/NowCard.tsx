8a4c03654334c20707305bcee0b71eaba121ea04
Daniil Gurski <daniilgurski6@gmail.com>
2026-04-27 23:56:50 +0200

    feat(web): populate card with actual requirement data

--- apps/web/src/components/NowCard.tsx
+++ apps/web/src/components/NowCard.tsx
@@ -1,3 +1,6 @@
+import { requirementQueryOptions } from "@/modules/requirement/api";
+import { useQuery } from "@tanstack/react-query";
+
 const mockRequirement = {
   id: "d7f4c32c-c356-476e-8551-6056e37da14c",
   title: "u12 - Projektarbete Chas Challenge",
@@ -177,34 +180,51 @@ const mockRequirement = {
 };
 
 export default function NowCard() {
-  const firstStep = mockRequirement.steps[0];
+  const { data: requirements } = useQuery(requirementQueryOptions.all);
+  const syncedRequirement = requirements?.find(
+    (req) => req.status === "COMPLETE",
+  );
 
-  // TODO: Use an actual step
+  if (!syncedRequirement) {
+    return <p> No synced requiremnets found !</p>;
+  }
+
+  const firstStep = syncedRequirement.steps?.find(
+    (step) => step.dependencyOrder === 1,
+  );
+
   return (
     <article className="bg-neutral-200 rounded-sm p-4 grid gap-y-6">
       <div className="divide-y divide-gray-400 grid gap-y-4">
         <header className="flex justify-between items-center pb-4">
-          <h2 className="text-2xl font-bold">{mockRequirement.title}</h2>
+          <div>
+            <h2 className="text-2xl font-bold">{syncedRequirement.title}</h2>
+            <p>
+              Time:
+              <span>{firstStep?.estimatedMinutes} min</span>, Complexity:
+              <span>{firstStep?.complexity}</span>
+            </p>
+          </div>
 
           <span className="bg-neutral-400 p-2 rounded-full">
-            {firstStep.category}
+            {syncedRequirement.type}
           </span>
         </header>
 
         <div className="grid gap-y-2">
           <section>
             <h3 className="font-medium text-xl">Action:</h3>
-            <p>{firstStep.action}</p>
+            <p>{firstStep?.action}</p>
           </section>
 
           <section>
             <h3 className="font-medium text-xl">Outcome:</h3>
-            <p>{firstStep.outcomeDefinition}</p>
+            <p>{firstStep?.outcomeDefinition}</p>
           </section>
 
           <section>
             <h3 className="font-medium text-xl">Quick Hint:</h3>
-            <p>{firstStep.quickStartLinkHint}</p>
+            <p>{firstStep?.quickStartLinkHint}</p>
           </section>
         </div>
       </div>
