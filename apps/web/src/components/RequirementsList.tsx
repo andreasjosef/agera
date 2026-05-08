f951370a1fcf7e645da462b258e7593a23474d74
Andreas Stratmann <hello.andreas@protonmail.com>
2026-05-08 19:20:13 +0200

    feat(web): add application menu component and add update req list

--- apps/web/src/components/RequirementsList.tsx
+++ apps/web/src/components/RequirementsList.tsx
@@ -1,4 +1,5 @@
 import { Requirement } from "@ccpilot/domain";
+import { Link } from "@tanstack/react-router";
 
 interface RequirementsListProps {
   requirements: Requirement[];
@@ -8,18 +9,27 @@ export default function RequirementsList({
   requirements,
 }: RequirementsListProps) {
   return (
-    <ul>
-      {requirements.map((requirement) => (
-        <li
-          className="flex items-center justify-between bg-app-surface p-2 mb-2"
-          key={requirement.id}
-        >
-          <div>
-            <h2>Uppgift: {requirement.title}</h2>
-            <p>Due: {new Date(requirement.due).toLocaleDateString()}</p>
-          </div>
-        </li>
-      ))}
-    </ul>
+    <>
+      <nav>
+        <button className="primary-button bg-app-surface-raised text-content-main">
+          Alla
+        </button>
+      </nav>
+      <ul className="grid grid-cols-3 gap-4">
+        {requirements.map((requirement) => (
+          <li
+            className="flex items-center justify-between bg-app-surface p-2 mb-2"
+            key={requirement.id}
+          >
+            <Link className="hover:cursor-pointer" to={requirement.id}>
+              <div>
+                <h2 className="font-semibold">{requirement.title}</h2>
+                <p>Due: {new Date(requirement.due).toLocaleDateString()}</p>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
