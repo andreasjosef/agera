10e3b25faae7c66e3d91cfdbe653297a3a5b0d87
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-09 10:50:20 +0200

    feat(forms): improve validation and error handling for auth forms, add login schema

--- apps/web/src/components/SignupForm.tsx
+++ apps/web/src/components/SignupForm.tsx
@@ -1,8 +1,7 @@
 import { Link } from "@tanstack/react-router";
-import { useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { signUpformSchema, type SignUpForm } from "@/Schemas/signUpFormSchema";
-
+import { signUpFormSchema, type SignUpForm } from "@/Schemas/formSchema";
 
 export interface SignupFormProps {
   onSubmit: (data: SignUpForm) => void;
@@ -10,47 +9,58 @@ export interface SignupFormProps {
   error?: string;
 }
 
-export default function SignupForm({onSubmit, isLoading,}: SignupFormProps) {
-  
+export default function SignupForm({
+  onSubmit,
+  isLoading,
+  error,
+}: SignupFormProps) {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<SignUpForm>({
-    resolver: zodResolver(signUpformSchema),
+    resolver: zodResolver(signUpFormSchema),
+    mode: "onChange",
+    reValidateMode: "onBlur",
   });
 
   return (
     <div className="flex flex-col gap-y-4 w-full max-w-sm">
       <h2>Signup</h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col w-sm gap-2">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col w-sm gap-2"
+      >
+        {error && (
+          <p className="bg-red-100 text-red-600 border border-red-400 p-2 rounded">
+            {error}
+          </p>
+        )}
         <input
-        {...register("name")}
+          {...register("name")}
           placeholder="What Should We Call You?"
           className="border-app-border border-2 p-2"
           type="text"
         />
-        <p>{errors.name?.message}</p>
+        <p className="text-red-500 text-sm">{errors.name?.message}</p>
 
         <input
-        {...register("email")}
+          {...register("email")}
           placeholder="Please Enter Your Email"
           className="border-app-border border-2 p-2"
           type="email"
         />
 
-        <p>{errors.email?.message}</p>
+        <p className="text-red-500 text-sm">{errors.email?.message}</p>
 
         <input
-        {...register("password")}
+          {...register("password")}
           placeholder="Choose a Password"
           className="border-app-border border-2 p-2"
           type="password"
         />
 
-        <p>{errors.password?.message}</p>
-
-
+        <p className="text-red-500 text-sm">{errors.password?.message}</p>
 
         <button disabled={isLoading} className="primary-button" type="submit">
           {isLoading ? "Signing you in..." : "Sign Up"}
