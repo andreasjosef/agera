9889a9ddb1236d4d5d4df8cfb5750aa72e34963f
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-12 20:35:58 +0200

    chore(ui): change to none default export

--- packages/ui/src/layouts/AppLayout.tsx
+++ packages/ui/src/layouts/AppLayout.tsx
@@ -1,13 +1,11 @@
 interface AppLayoutProps {
-  sidebar: React.ReactNode;
   children?: React.ReactNode;
 }
 
-export default function AppLayout({ sidebar, children }: AppLayoutProps) {
+export function AppLayout({ children }: AppLayoutProps) {
   return (
     <main className="h-screen grid grid-cols-[auto_1fr] overflow-hidden">
-      <aside>{sidebar}</aside>
-      <div className="p-4">{children}</div>
+      {children}
     </main>
   );
 }
