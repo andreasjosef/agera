8e192c1bb904e3f6d63898336365bd82711e981d
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-14 14:39:10 +0200

    feat(persistence): implement user status table, repo

---
+++ packages/persistence/src/users/schema.ts
@@ -0,0 +1,10 @@
+import * as t from "drizzle-orm/pg-core";
+import { user } from "../db/schema";
+
+export const userStatusTable = t.pgTable("status", {
+  user_id: t
+    .text("user_id")
+    .primaryKey()
+    .references(() => user.id),
+  timer_active: t.boolean("timer_active").notNull().default(false),
+});
