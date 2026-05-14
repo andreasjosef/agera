7242f3b21a8d7fc22b78d1bfa9561524160f915b
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-14 14:42:23 +0200

    feat(express): implemnet status router

---
+++ apps/express/src/routes/status/handlers/index.ts
@@ -0,0 +1,1 @@
+export { updateTimerStatus } from "./updateTimerStatus";
---
+++ apps/express/src/routes/status/handlers/updateTimerStatus.ts
@@ -0,0 +1,6 @@
+import { fail } from "@ccpilot/domain";
+
+export const updateTimerStatus = () => {
+  console.log("[EXPRESS] status update");
+  return fail("TODO");
+};
