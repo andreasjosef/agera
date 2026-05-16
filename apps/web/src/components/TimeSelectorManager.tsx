f0cf1f03ef3c637b9e309df2b710080c901d3579
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-16 13:57:11 +0200

    chore(web): use timer store

--- apps/web/src/components/TimeSelectorManager.tsx
+++ apps/web/src/components/TimeSelectorManager.tsx
@@ -1,16 +1,19 @@
-import { useCockpitSettings } from "@/modules/cockpit/store";
+import { useTimer } from "@/modules/cockpit/store";
 import { TimeSelector } from "@ccpilot/ui";
 import { useShallow } from "zustand/react/shallow";
 
 export default function TimeSelectorManager() {
-  const { timeMs, setTime } = useCockpitSettings(
-    useShallow((state) => ({ timeMs: state.timeMs, setTime: state.setTime })),
+  const { baseTime, setBaseTime } = useTimer(
+    useShallow((state) => ({
+      baseTime: state.baseTime,
+      setBaseTime: state.setBaseTime,
+    })),
   );
 
   return (
     <TimeSelector
-      timeMs={timeMs}
-      setTime={setTime}
+      timeSeconds={baseTime}
+      setTime={setBaseTime}
       handleSelectSubmit={(e) => {
         e.preventDefault();
         console.log("TODO: Update planning");
