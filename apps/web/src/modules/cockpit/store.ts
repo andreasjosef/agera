489aee1fe2fade9eb9034c0b626a32e1a3fc95fc
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-16 13:56:31 +0200

    feat(web): move selected time and pomodoro states into a seperate store

--- apps/web/src/modules/cockpit/store.ts
+++ apps/web/src/modules/cockpit/store.ts
@@ -1,11 +1,39 @@
 import { create } from "zustand";
 
-interface CockpitSettingsState {
-  timeMs: number;
-  setTime: (timeMs: number) => void;
+interface TimerState {
+  // Setup (Cockpit)
+  baseTime: number;
+
+  // Execution
+  mode: "focus" | "break";
+  isPaused: boolean;
+  timeRemainingSeconds: number;
+  cyclesRemaining: number;
+
+  setBaseTime: (seconds: number) => void;
+  setTimeRemainingSeconds: (seconds: number) => void;
+  setIsPaused: (isPaused: boolean) => void;
+  toggleMode: () => void;
 }
 
-export const useCockpitSettings = create<CockpitSettingsState>((set) => ({
-  timeMs: 30 * 60 * 1000,
-  setTime: (timeMs) => set({ timeMs }),
+export const useTimer = create<TimerState>((set) => ({
+  baseTime: 30 * 60,
+  mode: "focus",
+  isPaused: true,
+  timeRemainingSeconds: 25 * 60,
+  cyclesRemaining: 0,
+
+  setBaseTime: (seconds) => set({ baseTime: seconds }),
+  setTimeRemainingSeconds: (seconds) => set({ timeRemainingSeconds: seconds }),
+  setIsPaused: (isPaused) => set({ isPaused }),
+  toggleMode: () => {
+    return set((state) => {
+      const mode = state.mode;
+      return {
+        mode: mode === "focus" ? "break" : "focus",
+        timeRemainingSeconds: mode === "focus" ? 5 * 60 : 25 * 60,
+        isPaused: true,
+      };
+    });
+  },
 }));
