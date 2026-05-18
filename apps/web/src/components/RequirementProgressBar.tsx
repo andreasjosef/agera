75c9c5a1588385ea853378e46a4c8c4a56b4e66c
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-18 23:46:15 +0200

    feat(web): add progress tracking for requirement steps

---
+++ apps/web/src/components/RequirementProgressBar.tsx
@@ -0,0 +1,49 @@
+import { Requirement } from "@ccpilot/domain";
+import { CheckCircle2, Rocket } from "lucide-react";
+
+interface RequirementProgressProps {
+  requirement: Requirement | undefined;
+}
+
+const RequirementProgressBar = ({ requirement }: RequirementProgressProps) => {
+  if (!requirement) return null;
+
+  const totalSteps = requirement.steps.length;
+  const completedSteps = requirement.steps.filter(
+    (step) => step.complete,
+  ).length;
+
+  const progress = totalSteps === 0 ? 0 : (completedSteps / totalSteps) * 100;
+
+  return (
+    <div className="w-2/7 space-y-2">
+      <div className="text-sm font-medium flex items-center gap-2.5">
+        {progress === 100 ? (
+          <>
+            <CheckCircle2
+              className="h-4 w-4 text-green-500 "
+              strokeWidth={2.3}
+            />
+            <span>Alla steg är avklarade!</span>
+          </>
+        ) : (
+          <>
+            <Rocket className="text-violet-500 h-4 w-4" strokeWidth={2.3} />
+            <span>
+              {completedSteps} / {totalSteps} steg klara
+            </span>
+          </>
+        )}
+      </div>
+
+      <div className="h-3 w-full rounded bg-gray-200 overflow-hidden">
+        <div
+          className="h-full bg-electric-violet-400 transition-all duration-300"
+          style={{ width: `${progress}%` }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RequirementProgressBar;
