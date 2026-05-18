77cdaf49abe3f0848d40515b94b8d858985c94e3
Andreas Stratmann <hello.andreas@protonmail.com>
2026-05-18 15:49:39 +0200

    feat(web): create requirement links and upcoming steps widget

--- apps/web/src/routes/app.cockpit.tsx
+++ apps/web/src/routes/app.cockpit.tsx
@@ -1,13 +1,11 @@
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
+import { requirementQueryOptions } from "@/modules/requirement/api";
+
+import { Button } from "@ccpilot/ui";
 
 import NowDashboardLayout from "@/components/layouts/NowDashboardLayout";
-
-import { Button, UpcomingSteps } from "@ccpilot/ui";
-
-import Heatmap from "@/components/Heatmap";
-import { requirementQueryOptions } from "@/modules/requirement/api";
-import { useSuspenseQuery } from "@tanstack/react-query";
 import TimeSelectorManager from "@/components/TimeSelectorManager";
+import { UpcomingStepsSection } from "@/components/UpcomingStepsWidget";
 
 export const Route = createFileRoute("/app/cockpit")({
   component: RouteComponent,
@@ -16,18 +14,16 @@ export const Route = createFileRoute("/app/cockpit")({
 });
 
 function RouteComponent() {
-  const { data: preview } = useSuspenseQuery(requirementQueryOptions.preview);
   const navigate = useNavigate();
 
   return (
     <div className="h-full mx-auto max-w-6xl">
       <NowDashboardLayout>
         <div className="flex flex-col items-end gap-y-2">
-          <UpcomingSteps steps={preview} />
+          <UpcomingStepsSection />
           <Button onClick={() => navigate({ to: "/app" })}> Lift Off </Button>
         </div>
         <TimeSelectorManager />
-        <Heatmap />
       </NowDashboardLayout>
     </div>
   );
