eb2fda18f98d8a62650932bfcdd2366f2d047874
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-18 15:22:07 +0200

    feat(web): display requirement cards list

--- apps/web/src/routes/app.requirements.index.tsx
+++ apps/web/src/routes/app.requirements.index.tsx
@@ -1,4 +1,4 @@
-import { RequirementsList } from "@ccpilot/ui";
+import { RequirementCardLink } from "@/components/RequirementCardLink";
 import { requirementQueryOptions } from "@/modules/requirement/api";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
@@ -11,5 +11,23 @@ export const Route = createFileRoute("/app/requirements/")({
 
 function RouteComponent() {
   const { data: requirements } = useSuspenseQuery(requirementQueryOptions.all);
-  return <RequirementsList requirements={requirements} />;
+  return (
+    <>
+      <nav className="mb-8">
+        <button className="primary-button bg-app-surface-raised text-content-main font-medium text-3xl">
+          Mina Uppdrag
+        </button>
+      </nav>
+
+      <ul className="grid grid-cols-3 gap-5">
+        {requirements
+          .sort((a, b) => new Date(a.due).getTime() - new Date(b.due).getTime())
+          .map((requirement, index) => (
+            <li key={requirement.id}>
+              <RequirementCardLink requirement={requirement} index={index} />
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
