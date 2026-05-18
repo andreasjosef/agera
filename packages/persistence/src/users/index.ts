453bd7790ee44b0e10fb24f1bf63d7a057de459c
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-18 13:49:30 +0200

    feat(persistence): implement get active timer count method

--- packages/persistence/src/users/index.ts
+++ packages/persistence/src/users/index.ts
@@ -1,10 +1,12 @@
 import { type IUserStatusRepository } from "@ccpilot/domain";
 import { toggleTimerActive } from "./methods/toggleTimerActive.ts";
 import { getTimerActive } from "./methods/getTimerActive.ts";
+import { getActiveTimerCount } from "./methods/getActiveTimerCount.ts";
 
 export const createUserStatusRepository = (): IUserStatusRepository => {
   return {
     toggleTimerActive,
     getTimerActive,
+    getActiveTimerCount,
   };
 };
