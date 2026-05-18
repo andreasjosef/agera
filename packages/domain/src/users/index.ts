7a42d78aac742fec5b7fd116b5a1e466215c5279
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-18 13:53:12 +0200

    fix(domain): change export file name

--- packages/domain/src/users/index.ts
+++ packages/domain/src/users/index.ts
@@ -1,4 +1,4 @@
 export * from "./definitions.ts";
-export * from "./actions/toggleTimerActiveAction.ts";
-export * from "./actions/getTimerActiveAction.ts";
+export * from "./actions/toggleTimerActive.ts";
+export * from "./actions/getTimerActive.ts";
 export * from "./actions/getActiveTimerCount.ts";
