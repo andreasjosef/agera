6133d19ebee98695dc3f056f58d0a1bb2726be45
Andreas Stratmann <hello.andreas@protonmail.com>
2026-05-18 15:46:15 +0200

    feat(ui): expose RequirementLink primitive

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -16,3 +16,4 @@ export * from "./primitives/Button";
 export * from "./primitives/Error";
 export * from "./primitives/Card";
 export * from "./primitives/NavItem";
+export * from "./primitives/RequirementAnchor";
