a981c246bbff48b4612d6b72a8c2eca2f3bd376b
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-19 18:34:14 +0200

    feat(web): add icon only prop on nav link component

--- apps/web/src/components/NavLink.tsx
+++ apps/web/src/components/NavLink.tsx
@@ -6,14 +6,20 @@ export interface NavLinkProps {
   to: string;
   label: string;
   icon: LucideIcon;
+  iconOnly?: boolean;
 }
 
-export function NavLink({ to, label, icon }: NavLinkProps) {
+export function NavLink({ to, label, icon, iconOnly = false }: NavLinkProps) {
   // NOTE: Just discovered this pattern below -> Function as Child ... pretty epic!
   return (
     <Link to={to}>
       {({ isActive }) => (
-        <NavItem label={label} icon={icon} isActive={isActive} />
+        <NavItem
+          label={label}
+          icon={icon}
+          isActive={isActive}
+          iconOnly={iconOnly}
+        />
       )}
     </Link>
   );
