d6098968916b37cd0f224e3e1674a0018e0d3918
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-20 13:09:52 +0200

    feat(web): add classname prop to logout button, set as secondary

--- apps/web/src/components/LogoutButton.tsx
+++ apps/web/src/components/LogoutButton.tsx
@@ -1,15 +1,21 @@
 import { useSignOut } from "@/modules/auth/hooks";
 import { Button } from "@ccpilot/ui";
 
-export const LogoutButton = () => {
+interface LogoutButtonProps {
+  className?: string;
+}
+
+export const LogoutButton = ({ className }: LogoutButtonProps) => {
   const { logout, isLoggingOut } = useSignOut();
 
   return (
     <Button
       children="Log Out"
+      variant="secondary"
       onClick={() => logout()}
       disabled={isLoggingOut}
       isLoading={isLoggingOut}
+      className={className}
     />
   );
 };
