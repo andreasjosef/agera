027be7d7c5e2279a5e355c01026f56e713ec70e2
Andreas Stratmann <hello.andreas@protonmail.com>
2026-05-25 16:56:23 +0200

    feat(ui): expose welcome card component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -20,6 +20,7 @@ export * from "./features/BackButton";
 export * from "./features/ConnectionGuide";
 export * from "./features/EnergySelector";
 export * from "./features/StepFilter";
+export * from "./features/WelcomeCard";
 
 // Primitives
 export * from "./primitives/Button";
