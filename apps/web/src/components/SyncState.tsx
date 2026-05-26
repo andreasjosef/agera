0f36a5bfde42f7c6c498c23943c3a587230e7c33
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-26 11:35:20 +0200

    feat(web): new styles for sync state component

--- apps/web/src/components/SyncState.tsx
+++ apps/web/src/components/SyncState.tsx
@@ -1,5 +1,7 @@
 import { integrationQueries } from "@/modules/integrations/api";
 import { useSyncPolling } from "@/modules/integrations/hooks";
+import { SyncStatus } from "@ccpilot/domain";
+import { mergeStyles } from "@ccpilot/ui";
 import { useQuery } from "@tanstack/react-query";
 
 export default function SyncState() {
@@ -8,14 +10,38 @@ export default function SyncState() {
   );
   const { pollingData } = useSyncPolling("CANVAS", integration?.status);
 
-  if (integration?.status === "NOT_FOUND") {
-    return;
+  const getStatusMeta = (
+    status?: SyncStatus,
+  ): { color: string; message: string } => {
+    switch (status) {
+      case "COMPLETE":
+      case "IDLE":
+        return { color: "bg-state-success", message: "Stabil koppling" };
+      case "INITIALIZED":
+      case "PROCESSING":
+        return { color: "bg-state-caution", message: "Synkar..." };
+      case "ERROR":
+        return { color: "bg-state-danger", message: "Synk fel" };
+
+      default:
+        return { color: "bg-cod-500", message: "Okänd" };
+    }
+  };
+
+  if (!integration || integration.status === "NOT_FOUND") {
+    return null;
   }
 
-  /* TODO: Make this less distracting */
   return (
-    <p className="bg-canvas-primary text-app-bg p-2 rounded-2xl">
-      Sync Status: {pollingData?.status}
-    </p>
+    <div className="border-state-info border rounded-2xl p-2 flex items-center gap-x-2 shadow-[0px_2px_0px] shadow-state-info/50 bg-state-">
+      <span
+        className={mergeStyles(
+          "pulse-dot",
+          getStatusMeta(pollingData?.status).color,
+        )}
+        aria-hidden
+      ></span>
+      <span>{getStatusMeta(pollingData?.status).message}</span>
+    </div>
   );
 }
