da5069fe732e141263a738cb776857dbeb0bc3ca
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-27 16:52:12 +0200

    feat(ui)add optional link prop

--- packages/ui/src/primitives/GuideStepItem.tsx
+++ packages/ui/src/primitives/GuideStepItem.tsx
@@ -4,11 +4,12 @@ interface GuideStepItemProps {
   index: number;
   title: string;
   content: string;
+  link?: string
 }
 
-export function GuideStepItem({ index, title, content }: GuideStepItemProps) {
+export function GuideStepItem({ index, title, content, link }: GuideStepItemProps) {
   return (
-    <Card className="grid gap-y-2">
+    <Card className="grid gap-5">
       <header className="flex gap-x-2 items-center">
         <div className="size-10 rounded-sm border-brand-primary border text-brand-primary font-display flex items-center justify-center text-sm font-bold">
           {index}
@@ -16,6 +17,8 @@ export function GuideStepItem({ index, title, content }: GuideStepItemProps) {
         <h3 className="font-medium">{title}</h3>
       </header>
       <p className="text-content-muted">{content}</p>
+
+      <div>{link}</div> 
     </Card>
   );
 }
