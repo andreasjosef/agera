bcdb212eae9f4b66a5244fa28cec248e5fb44546
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-27 17:13:12 +0200

    styling(web): translate text

--- apps/web/src/routes/app.onboarding.tsx
+++ apps/web/src/routes/app.onboarding.tsx
@@ -13,11 +13,12 @@ function OnboardingPage() {
   return (
     <div className="grid max-w-220 mx-auto gap-5">
       <header className="mt-18">
-        <h2 className="text-xl mb-2 font-bold">Welcome {user?.name}</h2>
+        <h2 className="text-xl mb-2 font-bold">Välkommen {user?.name}</h2>
         <p className="text-content-muted">
-          This app is designed to integrate with your LMS system to help you
-          navigate your studies more easily. For the best experience please
-          select your provider from the list of supported LMS systems below.
+          Den här appen är utformad för att integreras med ditt LMS-system för
+          att hjälpa dig att navigera dina studier enklare. För bästa
+          upplevelse, välj din leverantör från listan över LMS-system som stöds
+          nedan.
         </p>
       </header>
       <section className="mt-2">
