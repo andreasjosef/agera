7dc9f3944a7edccba37458263f9c5acb83dfb0dd
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-27 11:59:21 +0200

    feat(web): adjust now layout

--- apps/web/src/components/layouts/NowDashboardLayout.tsx
+++ apps/web/src/components/layouts/NowDashboardLayout.tsx
@@ -6,15 +6,15 @@ interface NowDashboardLayoutProps {
 export default function NowDashboardLayout({
   children,
 }: NowDashboardLayoutProps) {
-  const [upcoming, timeSelector, bodyDoubling, liftOff] =
+  const [energy, upcoming, timeSelector, bodyDoubling] =
     Children.toArray(children);
   return (
     <div className="cockpit-grid h-full gap-2 items-start content-start">
+      <div className="h-full [grid-area:energy]">{energy}</div>
       <div className="h-full [grid-area:upcoming]">{upcoming}</div>
 
       <div className="[grid-area:time]">{timeSelector}</div>
-      <div className="[grid-area:body-doubling]">{bodyDoubling}</div>
-      <div className="[grid-area:lift-off] grid"> {liftOff} </div>
+      <div className="h-full [grid-area:body-doubling]">{bodyDoubling}</div>
     </div>
   );
 }
