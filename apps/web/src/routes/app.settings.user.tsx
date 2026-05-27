8939b1746dd51ef3c8c96b3d33e62b9a516ea1c3
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-05-27 15:14:30 +0200

    feat(web): add sidebar divider and  descriptions for settings sections

--- apps/web/src/routes/app.settings.user.tsx
+++ apps/web/src/routes/app.settings.user.tsx
@@ -10,7 +10,10 @@ function RouteComponent() {
 
   return (
     <div>
-      <h3 className="text-xl font-medium">Preferenser</h3>
+      <div className="flex flex-col gap-2 mb-5">
+        <h3 className="text-2xl font-medium">Preferenser</h3>
+        <p> Hantera dina personliga inställningar och upplevelser.</p>
+      </div>
 
       <ConfettiSwitch />
     </div>
