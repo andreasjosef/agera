2f944351d1246176fe493e4d22febe5847874f7e
Daniil Gurski <daniilgurski6@gmail.com>
2026-05-28 13:52:01 +0200

    fix(web): remove dublicate import

--- apps/web/src/routes/app.settings.integrations.tsx
+++ apps/web/src/routes/app.settings.integrations.tsx
@@ -1,5 +1,4 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { createFileRoute } from "@tanstack/react-router";
 
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { useEffect } from "react";
